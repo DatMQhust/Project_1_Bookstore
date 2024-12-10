560658289fbd7d1590db389d17292886d9d71625
DATMQHUST <Dat.mq225277@sis.hust.edu.vn>
2024-12-10 12:34:39 +0700

    category in admin

--- controllers/admin/category_controller.js
+++ controllers/admin/category_controller.js
@@ -29,3 +29,86 @@ module.exports.index = async (req,res)=>{
         numberBook: numberBooks
     })
 }
+module.exports.listProduct = async (req,res)=>{
+    const name = req.params.name;
+    const count = req.params.slg;
+    const {page,limit,offset,totalPage} = pageHelper(req.query,count)
+    let keyword = ""
+    if (req.query.search){
+        keyword= req.query.search
+    }
+    try {
+        const [id] = await db.execute('select categoryID from category where name = ?', [name]);
+        const categoryID = id[0]?.categoryID;
+      
+        if (!categoryID) {
+          return res.status(404).send('Không tìm thấy category với tên đã cho');
+        }
+      
+        const [product] = await db.execute(
+          'select * from product where categoryID = ? and name LIKE ? LIMIT ? OFFSET ?',
+          [categoryID, `%${keyword}%`,`${limit}`,`${offset}`]
+        );
+      
+        res.render("admin/page/product/index", {
+          products: product,
+          user: req.user,
+          totalPage: totalPage,
+          currentPage: page,
+        });
+      } catch (err) {
+        console.error('Lỗi:', err);
+        res.status(500).send('Có lỗi xảy ra trên server.');
+      }     
+}
+module.exports.add = async (req,res) =>{
+    const name = req.body.name;
+    const [cate] = await db.execute(`select * from category where name = ?`,[name]);
+    if (cate.length != 0){
+        return res.status(500).json({success : false,message : "Danh mục đã tồn tại"})
+    }
+    else {
+        try{
+            await db.execute(`insert into category(name) values (?)`,[name]);
+            return res.status(200).json({success : true,message : "Thêm danh mục thành công "})
+        }
+        catch(e) {
+            return res.status(401).json({success: false, message:"Có lỗi xảy ra vui lòng thử lại"})
+        }
+        
+    }
+}
+module.exports.delete = async (req,res) =>{
+    const id = req.body.id;
+    console.log(id)
+    const [result] = await db.execute(`select * from product where categoryID = ?`,[id]);
+    if (result.length != 0){
+        return res.status(500).json({success : false,message : "Danh mục đang chứa sản phẩm không thể xóa! Hãy xoá sản phẩm hoặc thay đổi danh mục sản phẩm trước khi xóa danh mục"})
+    }
+    try{
+        await db.execute(`delete from category where categoryID = ?`,[id]);
+        return res.status(200).json({success : true,message : "Xóa danh mục thành công "})
+    }
+    catch(e){
+        return res.status(401).json({success: false, message:"Có lỗi xảy ra vui lòng thử lại"})
+    }
+}
+module.exports.edit = async (req,res) =>{
+    const id = req.body.id;
+    const name = req.body.name;
+    console.log(id,name)
+    const [cate] = await db.execute(`select * from category where name = ?`,[name]);
+    // if(cate.length != 0){
+    //     return res.status(500).json({success : false,message : "Danh mục đã tồn tại"})
+    // }
+    // else{
+        try{
+            await db.execute(`update category set name = ? where categoryID = ?`,[name,id]);
+            return res.status(200).json({success : true,message : "Sửa danh mục thành công "})
+        }
+        catch(e){
+            return res.status(401).json({success: false, message:"Có lỗi xảy ra vui lòng thử lại"})
+        }
+    // }
+
+}
--- routes/admin/category_routes.js
+++ routes/admin/category_routes.js
@@ -8,5 +8,9 @@ const passportJWT = passport.authenticate("jwt-admin", {
     session: false,
 });
 router.get('/',passportJWT,controller.index)
+router.get('/:name/:slg',passportJWT,controller.listProduct)
+router.post('/add',passportJWT,controller.add)
+router.post('/delete',passportJWT,controller.delete)
+router.post('/edit',passportJWT,controller.edit)
 
 module.exports = router
